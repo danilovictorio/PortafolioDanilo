81abdd41fd8bb915398a7f1d7285923a306fbbde
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-06-13 18:40:33 +0200

    estilos y fuentes generales de la app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useState } from 'react';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Portfolio from './components/portfolio/portfolio';
+import Services from './components/services/Services';
+import Resume from './components/resume/Resume';
+
 import Sidebar from './components/sidebar/Sidebar';
 
 import './App.css';
@@ -10,27 +13,19 @@ function App() {
     <>
     <Sidebar />
     
+    <main className='main'>
 
+      <Home />
+      <About />
+      <Services />
+      <Resume />
+      <Portfolio />
 
-    
+    </main>
     </>
 
   );
 
-  function MyButton() {
-    const [count, setCount] = useState(0);
-  
-    function handleClick() {
-      setCount(count + 1);
-    }
-  
-    return (
-      <button onClick={handleClick}>
-        Clicked {count} times
-      </button>
-   
-    );
-  }
   
 }
 
