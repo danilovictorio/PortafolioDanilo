37f6c73835a958e518f61632314d1b2e6ad87f8e
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-08-02 11:32:42 +0200

    portafolio

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,27 @@
+import Work1 from "../../assets/work-1.svg";
+import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
+
+const Menu = [
+    {
+      id: 1,
+      image: Work1,
+      title: "Project Management Illustration",
+      category: "Design",
+    },
+    {
+      id: 2,
+      image: Work2,
+      title: "Guest App Walkthrough Screens",
+      category: "Art",
+    },
+    {
+      id: 3,
+      image: Work3,
+      title: "Delivery App Wireframe",
+      category: "Branding",
+    }
+  ];
+  
+
+export default Menu
